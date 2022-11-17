b0115403b2148d814e2785dd298666ecd1b2b94b
Carinaaa <xd.carina@yahoo.ro>
2022-11-17 13:09:56 +0200

    project update
    note that updaside down does not work

---
+++ app/src/main/java/laborator/sase/compas/MainActivity.java
@@ -0,0 +1,113 @@
+package laborator.sase.compas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.ActivityInfo;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private ImageView compass_image;
+    private TextView textView;
+
+    private SensorManager sensorManager;
+    private Sensor accelerometerSensor, magnetometerSensor;
+
+    private float[] lastAccelerometer = new float[3];
+    private float[] lastMagnetometer = new float[3];
+    private float[] rotationMatrix = new float[9];
+    private float[] orientation = new float[3];
+
+    boolean isLastAccelerometerArrayCopied = false;
+    boolean isLastMagnetometerArrayCopied = false;
+
+    long lastUpdatedTime = 0;
+    float currentDegree = 0f;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        compass_image = findViewById(R.id.compass_image);
+        textView = findViewById(R.id.xTextView);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        magnetometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this,magnetometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        sensorManager.unregisterListener(this, accelerometerSensor);
+        sensorManager.unregisterListener(this, magnetometerSensor);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if (sensorEvent.sensor == accelerometerSensor) {
+            System.arraycopy(sensorEvent.values, 0, lastAccelerometer, 0, sensorEvent.values.length);
+            isLastAccelerometerArrayCopied = true;
+        } else if (sensorEvent.sensor == magnetometerSensor) {
+            System.arraycopy(sensorEvent.values, 0, lastMagnetometer, 0, sensorEvent.values.length);
+            isLastMagnetometerArrayCopied = true;
+        }
+        if (isLastAccelerometerArrayCopied && isLastMagnetometerArrayCopied &&
+                System.currentTimeMillis() - lastUpdatedTime > 250) {
+            SensorManager.getRotationMatrix(rotationMatrix,null,lastAccelerometer,lastMagnetometer);
+            SensorManager.getOrientation(rotationMatrix, orientation);
+
+            float azimuthRadians = orientation[0];
+            float azimuthDegree = (float)Math.toDegrees(azimuthRadians);
+
+            if(azimuthDegree == 360.0f){
+                azimuthDegree = 0.0f;
+            }
+
+            if(azimuthDegree >= 179.0f){
+                azimuthDegree += (azimuthDegree - 179.0f);
+            }
+
+            RotateAnimation rotateAnimation = new RotateAnimation(currentDegree, -azimuthDegree,
+                    Animation.RELATIVE_TO_SELF, 0.5f,
+                    Animation.RELATIVE_TO_SELF, 0.5f);
+
+            rotateAnimation.setDuration(250);
+            rotateAnimation.setFillAfter(true);
+            compass_image.startAnimation(rotateAnimation);
+
+            currentDegree = -azimuthDegree;
+
+            lastUpdatedTime = System.currentTimeMillis();
+
+            int x = (int)azimuthDegree;
+            textView.setText(Math.abs(x) + "°");
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+}
